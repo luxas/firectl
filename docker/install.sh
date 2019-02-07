63e9fc44a1e962048cafe82b5ad9e4fb00082532
Lucas Käldström, NYLBR <lucas@luxaslabs.com>
2019-02-07 10:00:58 +0200

    Make the help output more descriptive for the install script

--- docker/install.sh
+++ docker/install.sh
@@ -2,6 +2,8 @@
 
 if [ ! -d /install ]; then
     echo "Please volume mount /install to the host directory you want firecracker and firectl copied to"
+    echo "Example: docker run -it -v /usr/local/bin:/install luxas/firectl"
+    echo "Alternatively, you can run /firectl and /firecracker directly from the container"
     exit 1
 fi
 
